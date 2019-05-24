dddfa0f33b38ec54861ba4181c7c6bde3d8daca9
kacho39 <kachunho1995@gmail.com>
2019-05-24 10:41:40 +0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,8 @@ module.exports = {
   client: 'mysql',
   connection: {
   	host : '13.112.44.83',
-    user: 'serverUser',
-    password: 'serverUser',
+    user: 'root',
+    password: 'Alvin1995!Mysql',
     database: 'scratchcard',
     multipleStatements: true
   }
